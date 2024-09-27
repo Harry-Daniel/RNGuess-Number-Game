8a39c316efdda6073e5780e68e3120c17908d562
Harry-Daniel <hayiborkomla@gmail.com>
2024-09-27 03:20:38 +0000

    Styling for Android & IOS

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,11 +1,10 @@
-
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
-    <TextInput/>
+    <View style={styles.inputContaner}>
+      <TextInput />
       <PrimaryButton> Reset </PrimaryButton>
       <PrimaryButton> Confirm </PrimaryButton>
     </View>
@@ -13,3 +12,20 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContaner: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius:8,
+    elevation:4, //Elevation is for android only
+    shadowColor:'black',
+    shadowOffset:{width:0,height:2},
+    shadowRadius:6,
+    shadowOpacity: 0.25,
+    
+
+  },
+});
