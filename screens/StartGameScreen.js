bd6099a9d80502394c0db1e4a4e446fa7dcbf679
Harry-Daniel <hayiborkomla@gmail.com>
2024-09-27 02:59:02 +0000

    Downgrading expo to older versions to enable app to function

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,11 @@
-import { TextInput, View } from "react-native-web";
+
+import { TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
     <View>
-      <TextInput />
+    <TextInput/>
       <PrimaryButton> Reset </PrimaryButton>
       <PrimaryButton> Confirm </PrimaryButton>
     </View>
