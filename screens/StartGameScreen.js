d304f9e80637f12e5b9925af25e2113b42dbea52
Harry-Daniel <hayiborkomla@gmail.com>
2024-09-27 03:31:27 +0000

    Styling the "Number Input" Element

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,7 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContaner}>
-      <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2} />
       <PrimaryButton> Reset </PrimaryButton>
       <PrimaryButton> Confirm </PrimaryButton>
     </View>
@@ -19,13 +19,22 @@ const styles = StyleSheet.create({
     marginHorizontal: 24,
     padding: 16,
     backgroundColor: "#72063c",
-    borderRadius:8,
-    elevation:4, //Elevation is for android only
-    shadowColor:'black',
-    shadowOffset:{width:0,height:2},
-    shadowRadius:6,
+    borderRadius: 8,
+    elevation: 4, //Elevation is for android only
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
     shadowOpacity: 0.25,
-    
-
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
