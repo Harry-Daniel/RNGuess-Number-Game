98f8af7574a9da6f441520bd8d8a5eb980a1835d
Harry-Daniel <hayiborkomla@gmail.com>
2024-10-04 04:01:29 +0000

    Introducing the Dimensions API

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 import Colors from "../../constants/colors";
 
 const Card = ({ children }) => {
@@ -7,19 +7,21 @@ const Card = ({ children }) => {
 
 export default Card;
 
-const styles=StyleSheet.create({
-    card: {
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 36,
-        marginHorizontal: 24,
-        padding: 16,
-        backgroundColor: Colors.primary800,
-        borderRadius: 8,
-        elevation: 4, //Elevation is for android only
-        shadowColor: "black",
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.25,
-      },
-})
+const deviceWidth = Dimensions.get("window").width;
+
+const styles = StyleSheet.create({
+  card: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: deviceWidth < 380 ? 18 : 36,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: Colors.primary800,
+    borderRadius: 8,
+    elevation: 4, //Elevation is for android only
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
